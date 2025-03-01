b280998edf60ce5ad2017bca89d0dc4b1b0b3c58
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-03-01 13:02:51 +0200

    Action delegates

--- GUI_thing/Form1.cs
+++ GUI_thing/Form1.cs
@@ -2,6 +2,10 @@ namespace GUI_thing
 {
     public partial class Form1 : Form
     {
+        //делегати дій кнопок
+        private Action actTransparency;
+        private Action actBackgroundColor;
+        private Action actMessage;
         public Form1()
         {
             InitializeComponent();
